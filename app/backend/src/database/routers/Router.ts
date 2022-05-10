2344c21d5600cee1ada2816f41b0bf2fb77b05d6
Faria <fariaguga@hotmail.com>
2022-05-10 15:18:20 -0300

    feat: add Login GET router

--- app/backend/src/database/routers/Router.ts
+++ app/backend/src/database/routers/Router.ts
@@ -1,9 +1,10 @@
 import * as express from 'express';
 import { inputValidation, formatValidation } from '../validation/login.validation';
 import { LoginController } from '../controllers/login.controller';
+import tokenAuth from '../validation/tokenAuth';
 
 const Router = express.Router();
 
 Router.post('/', inputValidation, formatValidation, LoginController.login);
-
+Router.get('/validate', tokenAuth, LoginController.getLogin);
 export default Router;
