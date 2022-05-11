c516e0518f4be377b0475fc68b3398de0fd68d90
Faria <fariaguga@hotmail.com>
2022-05-11 18:33:34 -0300

    feat: add matches/:id PATCH route

--- app/backend/src/database/routers/matches.router.ts
+++ app/backend/src/database/routers/matches.router.ts
@@ -8,5 +8,6 @@ const routerMatches = express.Router();
 routerMatches.get('/', MatchesController.getAll);
 routerMatches.post('/', matchesValidation, MatchesController.createMatches);
 routerMatches.patch('/:id/finish', MatchesController.finishMatches);
+routerMatches.patch('/:id', MatchesController.updateMatches);
 
 export default routerMatches;
